6f87c0bd09c5c4a3d4544ab6bdae134baf7ce3c4
Amir Ranjbar <iamirranjbar@gmail.com>
2024-12-17 11:23:16 +0330

    feature(GraphQL): configure graphql module and write graphql type, queries and mutations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { GraphqlModule } from './graphql/graphql.module';
+import { GraphQLDateTime } from 'graphql-scalars';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, GraphqlModule],
+  providers: [
+    {
+      provide: 'DateTime',
+      useValue: GraphQLDateTime,
+    },
+  ],
 })
 export class AppModule {}
