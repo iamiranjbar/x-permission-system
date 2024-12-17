ee0fdbd7e2d9eed2850df5a9a65feaabd3d3e789
Amir Ranjbar <iamirranjbar@gmail.com>
2024-12-17 21:55:44 +0330

    feature(User): add user entity and module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './core/database/database.module';
 import { GraphqlModule } from './core/graphql/graphql.module';
 import { GraphQLDateTime } from 'graphql-scalars';
+import { GroupModule } from './modules/group/group.module';
+import { TweetModule } from './modules/tweet/tweet.module';
+import { PermissionModule } from './modules/permission/permission.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [DatabaseModule, GraphqlModule],
+  // imports: [DatabaseModule, GraphqlModule, GroupModule, TweetModule, PermissionModule, UserModule],
+  imports: [DatabaseModule, GraphqlModule, UserModule],
   providers: [
     {
       provide: 'DateTime',
---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+
+const UserRepository = TypeOrmModule.forFeature([User]);
+@Module({
+  imports: [UserRepository],
+  exports: [UserRepository],
+})
+export class UserModule {}
