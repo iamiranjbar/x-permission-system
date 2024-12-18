02b2a47a5be037679a3855609edee61d8d6c2c36
Amir Ranjbar <iamirranjbar@gmail.com>
2024-12-18 12:55:18 +0330

    chore(Errors): add centralized error management configs.

---
+++ src/core/constants/errors.ts
@@ -0,0 +1,5 @@
+export const Errors = {
+  Group: {
+    EmptyMembersList: 'At least one of userIds or groupIds must be provided.'
+  }
+}
--- src/core/graphql/graphql.module.ts
+++ src/core/graphql/graphql.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLError } from 'graphql/error';
 
 @Module({
   imports: [
@@ -8,6 +9,16 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       driver: ApolloDriver,
       typePaths: ['src/core/graphql/*.graphql'],
       playground: true,
+      formatError: (error: GraphQLError) => {
+        console.log(error);
+        const statusCode = (error.extensions?.originalError as any)?.statusCode;
+        return {
+          message: error.message,
+          code: error.extensions?.code || 'INTERNAL_SERVER_ERROR',
+          path: error.path,
+          statusCode: statusCode,
+        };
+      },
     }),
   ],
 })
